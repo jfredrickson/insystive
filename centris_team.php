c337e24838219a6aa1a5c1c26cfcaa2bdfd07f7e
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2014-01-05 22:44:33 -0500

    new bios

--- centris_team.php
+++ centris_team.php
@@ -27,11 +27,14 @@
           
           <div class="row bio">
             <div class="col-md-2">
-              <img class="bio-image img-circle img-responsive" src="images/bio-justin.jpg" alt="Photo of Justin B. Brown">
+              <img class="bio-image img-circle img-responsive" src="images/bio-justin.jpg" alt="Photo of Justin Brown">
             </div>
             <div class="col-md-10">
-              <h2 class="bio-heading">Justin B. Brown <span class="text-muted">/ Founder and CEO</span></h2>
-              <p>Justin is a career public safety executive with experience in emergency health services, fire and rescue services, and emergency management. He is currently Deputy Commissioner of the Boston Fire Department where he oversees Information Technology, Training, Emergency Medical Services, and Strategic Planning. Together with the Fire Commissioner and Deputy Commissioner for Finance and Administration, Justin leads a Department with over 1,700 employees and an annual budget in excess of $230 million. Justin has previously served as a Battalion Fire Chief, paramedic, and firefighter in Washington, DC, and as Citywide Interagency Coordinator for the Field Operations Division of the New York City Office of Emergency Management. In addition to field operations, Justin's primary interest is in technology implementation for the public sector. Justin holds a BS in Health Sciences from George Washington University.</p>
+              <h2 class="bio-heading">Justin Brown <span class="text-muted">/ Founder and CEO</span></h2>
+              <p>Justin is the healthcare and operations lead on the team.  He is responsible for strategy and overall concept development. He has experience working with large IT projects, healthcare delivery systems and home health services.</p>
+              <p><b>From:</b> Guilford, CT</p>
+              <p><b>Fun Fact:</b> Justin has been admitted to the Yale School of Medicine twice. *</p>
+              <small class="text-muted">* Once for pancreatitis and once for an appendectomy. Neither admission resulted in a degree.</small>
             </div>
           </div>
           
@@ -55,9 +58,7 @@
             </div>
             <div class="col-md-10">
               <h2 class="bio-heading">Ryan O'Connor <span class="text-muted">/ UX/UI Lead</span></h2>
-              <p>Ryan is a User Experience researcher & designer. He is well-trained in usability principles and practices, and his work ranges from web sites to mobile designs and tablet applications. His usability work involves tests from expert audits to live user testing of websites, web apps, and mobile apps. He has completed his Masters coursework in Human Factors & Ergonomics and is currently completing his thesis.</p>
-              <p>Ryan's credo for design is "Structure Follows Strategy." As such, he
-systematically develops user-oriented products. Through background research, interviewing, prototyping, and usability testing, he creates precise, goal-directed products. His goal is to design a user's experience with the aim of bringing them back for more.</p>
+              <p>Ryan is our UX/UI lead and will be our primary development liaison with Cremalab. He will be responsible for ensuring designing the user flow with Cremalab and he will ensure appropriate testing during product development.</p>
             </div>
           </div>
           
@@ -68,12 +69,8 @@ systematically develops user-oriented products. Through background research, int
               <img class="bio-image img-circle img-responsive" src="images/bio-donald.jpg" alt="Photo of Donald R. Denning, Jr.">
             </div>
             <div class="col-md-10">
-              <h2 class="bio-heading">Donald R. Denning, Jr. <span class="text-muted">/ Senior Engineering Advisor</span></h2>
-              <p>Donald R. Denning, Jr. is the Chief Information Officer for Public Safety for the City of Boston and has been working in this role since May 2008. Donald is leading the development of a new, state-of-the-art computer-aided dispatch and records management system for the city's 911 operation. He is also responsible for public safety￼interoperability and is developing new standards for radio and network communications￼between city public safety offices and state and federal agencies.</p>
-              
-              <p>Donald is a member of several local, state, and national boards, including the Department of Homeland Security's Regional Emergency Communications Coordination Working Group (RECCWG), the Broadband Task Force, and the Assessment of Future Spectrum and Technology Working Groups.</p>
-              
-              <p>Donald transitioned to the public sector after working for two decades in the private sector as a Member of the Research Staff at HP Labs, Intel, Compaq and Digital Equipment Corporation where he secured several patents in communications and medical technology. Some of the products and tools are currently being used in the public and medical sectors. Donald is also a member of the Association of Former Intelligence Officers, the International Association of Chiefs of Police, and the Association of Public-Safety Communications Officials.</p>
+              <h2 class="bio-heading">Donald Denning <span class="text-muted">/ Engineering Advisor</span></h2>
+              <p>Don provides us with knowledge of devices and emerging device technology.  He is also our project management guru.  His knowledge of device technology, IT systems and communications is almost upsetting to us.</p>
             </div>
           </div>
           
@@ -84,7 +81,10 @@ systematically develops user-oriented products. Through background research, int
               <img class="bio-image img-circle img-responsive" src="images/bio-david.jpg" alt="Photo of David Hinderaker">
             </div>
             <div class="col-md-10">
-              <h2 class="bio-heading">David Hinderaker <span class="text-muted">/ Senior Business and Finance Advisor</span></h2>
+              <h2 class="bio-heading">David Hinderaker <span class="text-muted">/ Business Advisor</span></h2>
+              <p>David specializes in understanding the customer need and utilizing customer feedback to drive business and product development.  He brings tremendous business development, sales and marketing experience.</p>
+              <p><b>From:</b> Minneapolis, MN</p>
+              <p><b>Fun Fact:</b> Dave built a robot, although it doesn’t really work yet.</p>
             </div>
           </div>
           
