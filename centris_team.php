5fcf2fd1336b9a44021775481843b0f43cb52efb
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2014-01-06 11:37:17 -0500

    update bios and add linkedin

--- centris_team.php
+++ centris_team.php
@@ -34,6 +34,7 @@
               <p>Justin is the healthcare and operations lead on the team.  He is responsible for strategy and overall concept development. He has experience working with large IT projects, healthcare delivery systems and home health services.</p>
               <p><b>From:</b> Guilford, CT</p>
               <p><b>Fun Fact:</b> Justin has been admitted to the Yale School of Medicine twice. *</p>
+              <div class="linkedin"><a href="http://www.linkedin.com/pub/justin-brown/5a/536/975/"><img src="images/linkedin.png" alt="LinkedIn"><span>LinkedIn Profile</span></a></div>
               <small class="text-muted">* Once for pancreatitis and once for an appendectomy. Neither admission resulted in a degree.</small>
             </div>
           </div>
@@ -48,6 +49,7 @@
               <h2 class="bio-heading">Ipsheeta Furtado <span class="text-muted">/ COO</span></h2>
               <p>Ipsheeta is an engineer and serial entrepreneur. She has built her own startups, assisted others and won several awards along the way.  She brings a wealth of experience in technology, business and management, and we are excited to have her as a critical addition to our talented team.</p>
               <p><b>From:</b> Manhattan, KS (The Little Apple)</p>
+              <div class="linkedin"><a href="http://www.linkedin.com/in/ipsheeta"><img src="images/linkedin.png" alt="LinkedIn"><span>LinkedIn Profile</span></a></div>
             </div>
           </div>
           
@@ -60,6 +62,8 @@
             <div class="col-md-10">
               <h2 class="bio-heading">Ryan O'Connor <span class="text-muted">/ UX/UI Lead</span></h2>
               <p>Ryan is our UX/UI lead and will be our primary development liaison with Cremalab. He will be responsible for designing the user flow with Cremalab, and he will ensure appropriate testing during product development.</p>
+              <p><b>From:</b> Long Beach, CA</p>
+              <div class="linkedin"><a href="http://www.linkedin.com/pub/ryan-o-connor/49/5b7/b26"><img src="images/linkedin.png" alt="LinkedIn"><span>LinkedIn Profile</span></a></div>
             </div>
           </div>
           
@@ -72,6 +76,9 @@
             <div class="col-md-10">
               <h2 class="bio-heading">Donald Denning <span class="text-muted">/ Engineering Advisor</span></h2>
               <p>Don provides us with knowledge of devices and emerging device technology.  He is also our project management guru.  His knowledge of device technology, IT systems and communications is almost upsetting to us.</p>
+              <p><b>From:</b> Boston, MA</p>
+              <p><b>Fun Fact:</b> Don owns a tractor.</p>
+              <div class="linkedin"><a href="http://www.linkedin.com/pub/donald-denning/37/623/30a"><img src="images/linkedin.png" alt="LinkedIn"><span>LinkedIn Profile</span></a></div>
             </div>
           </div>
           
@@ -86,6 +93,7 @@
               <p>David specializes in understanding the customer need and utilizing customer feedback to drive business and product development.  He brings tremendous business development, sales and marketing experience.</p>
               <p><b>From:</b> Minneapolis, MN</p>
               <p><b>Fun Fact:</b> Dave built a robot, although it doesn’t really work yet.</p>
+              <div class="linkedin"><a href="http://www.linkedin.com/pub/david-hinderaker/3/265/139"><img src="images/linkedin.png" alt="LinkedIn"><span>LinkedIn Profile</span></a></div>
             </div>
           </div>
           
