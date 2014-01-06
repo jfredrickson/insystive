7588aa8da7005742b6e639c70f1edc0b6452d0a1
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2014-01-06 11:38:31 -0500

    content updates

--- centris_project.php
+++ centris_project.php
@@ -24,7 +24,7 @@
           <div class="row featurette">
             <div class="col-md-8">
               <h2 class="featurette-heading">CentrIS. <span class="text-muted">Connecting medical device information to the people who need it, when they need it.</span></h2>
-              <p class="lead">CentrIS will connect caregivers with real-time alerts, notifications and data about the acute status of their patients and loved ones. It uses cutting-edge technology to bridge the gap between care levels and providing a means for those managing aging and chronic illness to remain safely independent.</p>
+              <p class="lead">CentrIS will connect caregivers with real-time alerts, notifications and data about the acute status of their patients and loved ones. It uses cutting-edge technology to bridge the gap between care levels and provide a means for those managing aging and chronic illness to remain safely independent.</p>
             </div>
             <div class="col-md-4">
               <img class="featurette-image img-responsive" src="images/centris-diagram.png" alt="General diagram of CentrIS system">
@@ -40,9 +40,8 @@
               </div>
             </div>
             <div class="col-md-8">
-              <h2 class="featurette-heading">How do you make people safer, healthier and more independent?</h2>
-              <p class="lead">In healthcare, independence is achieved by connecting real-time patient data (not just health records) to the people who need it most.  Although CentrIS can also be used for enterprise IT systems, it is initially being developed for the individual consumer.</p>
-              <p class="lead">CentrIS will offer safe, independent living for those managing aging, chronic disease or disability. We do this by connecting our customers' medical device information with their caregivers or loved ones.</p>
+              <h2 class="featurette-heading">Your data. <span class="text-muted">From your devices.  To your caregivers.</span></h2>
+              <p class="lead">In healthcare, independence can often be achieved by simply making sure people know how you're doing. CentrIS can let them know automatically. It uses your medical devices to send your body's information to the people who need to know most.</p>
             </div>
           </div>
           
@@ -52,7 +51,7 @@
             <div class="col-md-8">
               <h2 class="featurette-heading">Real-time. <span class="text-muted">Because you need data now.</h2>
               <p class="lead">Our system offers real-time, vendor-neutral communication between patients, devices and caregivers.</p>
-              <p class="lead">At Insystive, we don’t believe consumers should have to choose between cutting edge medical devices and cutting edge communications.  That’s why we have focused on making connecting existing devices in more useful ways.</p>
+              <p class="lead">At Insystive, we don’t believe consumers should have to choose between cutting edge medical devices and cutting edge communications.  That’s why we have focused on connecting existing devices in more useful ways.</p>
               <p class="lead">With CentrIS, Insystive is building an ecosystem of medical devices that puts the power of real-time health data in the hands of those who need it most.</p>
             </div>
             <div class="col-md-4">
