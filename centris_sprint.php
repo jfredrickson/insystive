29d23a518311365a8a2d08950baaeb461f1ce358
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2014-01-06 17:46:30 -0500

    add sprint and techstars logos

--- centris_sprint.php
+++ centris_sprint.php
@@ -22,7 +22,15 @@
         <div class="container">
           <section>
             <h1>Sprint Accelerator</h1>
+            
+            <p style="text-align: center">
+              <img src="images/sprint-logo.png" alt="Sprint logo">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <img src="images/techstars-logo.png" alt="Techstars logo">
+            </p>
 
+            <hr>
+            
             <p>Sprint and Techstars have combined to offer a unique opportunity for ten teams to build their business in Kansas City's famous Crossroads District. We strongly believe we have the right idea, the right team and just the right time to develop our product demo and help make people’s lives safer, healthier and more independent.</p>
 
             <p>Insystive is excited to apply to the 2014 Sprint Accelerator in Kansas City, MO. Insystive founder Justin Brown's family hails from Kansas City, and he spent his entire life traveling to and from the area. Justin considers Kansas City his second home and is thrilled to have this opportunity to help build the Silicon Prairie.</p>
