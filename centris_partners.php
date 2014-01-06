34205b669ddea48567872ca7b2e3a551af5c153c
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2014-01-06 17:41:17 -0500

    update partners and add logos

--- centris_partners.php
+++ centris_partners.php
@@ -23,21 +23,24 @@
           <section>
             <h1>CentrIS Partners</h1>
 
-            <p>Insystive has partnered with two outstanding companies to develop CentrIS.</p>
+            <p>Insystive is excited to partner with two extremely talented technology companies to build the CentrIS product. With these two strategic partnerships, Insystive is solidly positioned to build a high-quality product demo by June 2014. Keep reading for more information about our partners.</p>
+            
+            <hr>
 
-            <h2>HumanAPI</h2>
-
-            <p>HumanAPI is an API for human health data that allows users to easily collect, normalize and share their data across a variety of applications.</p>
-            <p>Their API service allows an application developer to easily connect multiple data sources relevant to their application by embedding code directly into their authentication flow. This, in turn, allows a user to much more seamlessly connect and share their health data (from a wide variety of sources) directly with that application, enriching their experience.</p>
-            <p>Their authentication flow embeds itself seamlessly into any application, ensuring that the user does not have to leave the experience they are accustomed to.</p>
-            <p>Check out <a href="http://www.humanapi.co/">www.humanapi.co</a> for more information.</p>
-
-            <h2>Cremalab</h2>
-
-            <p>Cremalab is passionate about connecting business and product development to amazing digital experiences.  They work as partners to create a constant shared understanding of the business and product assumptions, goals, and outcomes in order to measure success for each product iteration. They work iteratively through design and development in order to move features forward so that product assumptions can be validated as quickly as possible.</p>
-            <p>Cremalab's team provides all facets of digital product development. They are passionate about great user experience and creative user interfaces. They are experts in  Ruby on Rails development, Javascript application development (node.js, chaplin, etc.) and responsive front-end development for browsers of all sizes.</p>
-            <p>Their team does not go away to build products in isolation, but works collaboratively to build the right solutions with their partners.  Cremalab is passionate about building successful products not only for their partners, but also for the users and customers of their products. They desire to build products and businesses that scale, last and solve real problems.</p>
-            <p>Visit them at <a href="http://www.cremalab.com/">www.cremalab.com</a>.</p>
+            <div class="row featurette">
+              <div class="col-md-6">
+                <h2 style="text-align: center"><img src="images/cremalab-logo.png" alt="Cremalab Logo"></h2>
+                <p>Cremalab is passionate about connecting business and product development to amazing digital experiences.  They work as partners to create a constant shared understanding of the business and product assumptions, goals, and outcomes in order to measure success for each product iteration. They work iteratively through design and development in order to move features forward so that product assumptions can be validated as quickly as possible.</p>
+                <p>Cremalab's team provides all facets of digital product development. They are passionate about great user experience and creative user interfaces. They are experts in  Ruby on Rails development, Javascript application development (node.js, chaplin, etc.) and responsive front-end development for browsers of all sizes.</p>
+                <p>Their team does not go away to build products in isolation, but works collaboratively to build the right solutions with their partners.  Cremalab is passionate about building successful products not only for their partners, but also for the users and customers of their products. They desire to build products and businesses that scale, last and solve real problems.</p>
+                <p>Visit them at <a href="http://www.cremalab.com/">www.cremalab.com</a>.</p>
+              </div>
+              <div class="col-md-6">
+                <h2 style="text-align: center"><img src="images/humanapi-logo.png" alt="HumanAPI Logo"></h2>
+                <p>HumanAPI is an API for human health data that allows users to easily collect, normalize and share their data across a variety of applications. Their API service allows an application developer to easily connect multiple data sources relevant to their application by embedding code directly into their authentication flow. This, in turn, allows a user to much more seamlessly connect and share their health data (from a wide variety of sources) directly with that application, enriching their experience. Their authentication flow embeds itself seamlessly into any application, ensuring that the user does not have to leave the experience they are accustomed to.</p>
+                <p>Check out <a href="http://www.humanapi.co/">www.humanapi.co</a> for more information.</p>
+              </div>
+            </div>
           </section>
         </div>
       </main>
